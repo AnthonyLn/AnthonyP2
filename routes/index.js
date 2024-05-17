983070fb0c2c81187017e2ce3c32ee018150e1dc
AnthonyLn <enriquedanielx@gmail.com>
2024-05-17 12:14:08 -0400

    formulario

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
-var express = require('express');
+const ContactosController = require("../controllers/ContactosController");
+const contactosController = new ContactosController();
+
+const indexControllers = require("../controllers/IndexControllers")
+
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log['asdodo']
-  res.render('index', {
-     title: 'Informacion Personal',
-     FirstName : 'Anthony Enrique',
-     LastName : 'Arteaga Daniel',
-     Id : '30874360',
-     Section : '4'
-     });
-});
+router.get("/", indexControllers);
+
+router.post("/form-contacto", contactosController.add);
 
 module.exports = router;
